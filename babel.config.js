6a23c660d36a66d49d759dd079a2973cefbd6cc0
Harqer <aoolllathens@outlook.com>
2026-04-29 23:26:51 -0400

    fix: absolute cure (React 19 + RN 0.85 + Flow Strip) with zero compromises

--- babel.config.js
+++ babel.config.js
@@ -6,6 +6,7 @@ module.exports = function (api) {
       "nativewind/babel",
     ],
     plugins: [
+      "@babel/plugin-transform-flow-strip-types",
       "react-native-reanimated/plugin",
     ],
   };
